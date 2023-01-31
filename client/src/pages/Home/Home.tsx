a976caac5ab8e521c7cc9d1f15b24939c1aef5fd
Liioan <dev.davidbijok@gmail.com>
2023-01-31 21:30:30 +0100

    add hero section to home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 //. styles
 import styles from './Home.module.css';
 
+//. components
+import Hero from './Hero/Hero';
+
 export default function Home() {
   return (
     <div>
-      {/* */}
+      <Hero />
       {/* */}
     </div>
   );
