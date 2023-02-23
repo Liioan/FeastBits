0397b0f8a29563225c6a97e9ad272b7053a7c4bc
Liioan <dev.davidbijok@gmail.com>
2023-02-23 23:14:39 +0100

    add responsive desing for offer card list and offer card

--- client/src/pages/Diets/Diets.tsx
+++ client/src/pages/Diets/Diets.tsx
@@ -7,7 +7,7 @@ import styles from './Diets.module.css';
 
 export default function Diets() {
   return (
-    <main>
+    <main className={styles.diets}>
       <Header text={'Diets'} step={'h2'} />
       <OfferList isOnHomePage={false} path={'offer'} />
     </main>
