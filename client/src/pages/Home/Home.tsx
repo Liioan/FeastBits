bfcc6d69adda3ef7077f3e3c151edcb31fd4378d
Liioan <dev.davidbijok@gmail.com>
2023-02-24 21:47:18 +0100

    add link buttons to home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 //. styles
 import styles from './Home.module.css';
 
@@ -6,6 +8,7 @@ import Hero from './Hero/Hero';
 import BlogList from '../Blog/BlogList/BlogList';
 import OfferList from '../../components/OfferList/OfferList';
 import Header from '../../components/Header';
+import GradientButton from '../../components/Buttons/GradientButton';
 
 export default function Home() {
   return (
@@ -14,14 +17,23 @@ export default function Home() {
       <section className={styles.homeSection} id='recomendations'>
         <Header text={'Our recomendations'} step={'h2'} />
         <OfferList isOnHomePage={true} path={'homepageSubs'} />
+        <Link to={'/diets'}>
+          <GradientButton text='See other offers' width={36} />
+        </Link>
       </section>
       <section className={styles.homeSection}>
         <Header text={'Single delivery'} step={'h2'} />
         <OfferList isOnHomePage={true} path={'homepageSingle'} />
+        <Link to={'meals'}>
+          <GradientButton text='See other offers' width={36} />
+        </Link>
       </section>
       <section className={styles.homeSection}>
         <Header text='Our blog' step='h2' />
         <BlogList isOnHomePage={true} path={'homepageBlog'} />
+        <Link to={'blog'}>
+          <GradientButton text='See more posts' width={36} />
+        </Link>
       </section>
     </main>
   );
