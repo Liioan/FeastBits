09dbfb405fdbfc8db3825dc14e33901ab08bc122
Liioan <dev.davidbijok@gmail.com>
2023-04-19 19:40:49 +0200

    change font size in admin panel in orders section

--- client/src/pages/AdminPanel/Sections/Sections.tsx
+++ client/src/pages/AdminPanel/Sections/Sections.tsx
@@ -5,7 +5,6 @@ import { OfferData } from '../../../types/offer';
 import { OrderData } from '../../../types/order';
 import { useState, FormEvent } from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
 import { useAuth } from '../../../context/AuthContext';
 
 //. components
